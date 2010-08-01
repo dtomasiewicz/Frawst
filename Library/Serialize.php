d785331fe7d027fb501abbe0bbbf54db37145e92
Daniel Tomasiewicz <dtomasiewicz@gmail.com>
2010-08-01 05:35:45 -0700

    merged functions.php into core bootstrap (and removed functions.php)
    moved get_serial_class() to Frawst\Library\Serialize::getSerialClass()

---
+++ Library/Serialize.php
@@ -0,0 +1,20 @@
+<?php
+	namespace Frawst\Library;
+	
+	/**
+	 * This houses any serialization-oriented methods.
+	 */
+	class Serialize {
+		/**
+		 * Safely determines the type or classname of a serialized entity without
+		 * unserializing (or instantiating) it.
+		 * @param string $serial The serialized entity
+		 * @return string The type or class name the entity would unserialize to
+		 */
+		public static function getSerialClass($serial) {
+			$types = array('s' => 'string', 'a' => 'array', 'b' => 'bool', 'i' => 'int', 'd' => 'float', 'N;' => 'null');
+			
+			$parts = explode(':', $serial, 4);
+			return isset($types[$parts[0]]) ? $types[$parts[0]] : trim($parts[2], '"'); 
+		}
+	}
--- core.php
+++ core.php
@@ -1,119 +1,133 @@
 <?php
-	namespace Frawst;
-	
 	/**
 	 * Frawst Framework Version 0.1 (development)
 	 * Author: Daniel Tomasiewicz < www.fourstaples.com >
 	 * 
 	 * This is the main bootstrap file, through which everything happens.
 	 */
-	error_reporting(E_ALL);
-	define('Frawst\\VERSION', '0.1dev');
-	define('Frawst\\SCRIPT_START', microtime(true));
 	
-	/**
-	 * Required definitions
-	 */
-	defined('Frawst\\APP_NAME')
-		or define('Frawst\\APP_NAME', basename(dirname($_SERVER['SCRIPT_NAME'])));
-	defined('Frawst\\DOMAIN')
-		or define('Frawst\\DOMAIN', $_SERVER['HTTP_HOST']);
-	defined('Frawst\\ROOT')
-		or define('Frawst\\ROOT', dirname(__FILE__));	
-	defined('Frawst\\APP_ROOT')
-		or define('Frawst\\APP_ROOT', dirname(ROOT).DIRECTORY_SEPARATOR.APP_NAME);
-	defined('Frawst\\WEB_ROOT')
-		or define('Frawst\\WEB_ROOT', dirname($_SERVER['SCRIPT_NAME']));
-	defined('Frawst\\AJAX_SUFFIX')
-		or define('Frawst\\AJAX_SUFFIX', '___AJAX___');
-	
-	require('Loader.php');
-	require('functions.php');
-	
-	/**
-	 * Paths in which libraries can be held.
-	 */
-	Loader::addPath(ROOT, 'Frawst', 'core');
-	Loader::addPath(ROOT.DIRECTORY_SEPARATOR.'vendor', '*', 'core');
-	Loader::addPath(APP_ROOT, 'Frawst', 'app');
-	Loader::addPath(APP_ROOT.DIRECTORY_SEPARATOR.'vendor', '*', 'app');
-	//@todo this probably should be in an app-specific bootstrap since it's specific to Corelativ
-	Loader::addPath(APP_ROOT.DIRECTORY_SEPARATOR.'Model', 'Corelativ\Model', 'app');
-	
-	/**
-	 * App-specific bootstrapping
-	 */
-	if(file_exists($file = APP_ROOT.DIRECTORY_SEPARATOR.'bootstrap.php')) {
-		require($file);
-	}
-	
-	date_default_timezone_set(Config::read('general.timezone'));
-	setlocale(LC_ALL, Config::read('general.locale'));
-	
-	$data = null;
-	$mapper = null;
-	$cache = null;
-	if($cacheConfig = Config::read('cache')) {
-		$c = $cacheConfig['controller'];
-		$cache = new $c($cacheConfig);
-	}
-	if($dataConfig = Config::read('data')) {
-		$c = $dataConfig['controller'];
-		$data = new $c($dataConfig, $cache);
-	}
-	if($ormConfig = Config::read('orm')) {
-		$c = $ormConfig['mapper'];
-		$mapper = new $c($ormConfig, $data, $cache);
-	}
-	
-	$method = $_SERVER['REQUEST_METHOD'];
-	if($method == 'GET') {
-		$requestData = $_GET;
-	} elseif($method == 'POST') {
-		$requestData = $_POST;
-	} else {
-		$requestData = array();
-		parse_str(file_get_contents('php://input'), $requestData);
-	}
-	
-	// REST hack for browsers that don't support PUT and DELETE methods
-	if(isset($requestData['___METHOD'])) {
-		$method = $requestData['___METHOD'];
-		unset($requestData['___METHOD']);
-	}
-	
-	/**
-	 * I HATE YOU MAGIC QUOTES
-	 */
-	if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
-	 	function stripslashes_deep($value) {
-			$value = (is_array($value)) ?
-				array_map('stripslashes_deep', $value) :
-				stripslashes($value);
-			return $value;
+	namespace {
+		error_reporting(E_ALL);
+		define('Frawst\\VERSION', '0.1dev');
+		define('Frawst\\SCRIPT_START', microtime(true));
+		
+		/**
+		 * Required definitions
+		 */
+		defined('Frawst\\APP_NAME')
+			or define('Frawst\\APP_NAME', basename(dirname($_SERVER['SCRIPT_NAME'])));
+		defined('Frawst\\DOMAIN')
+			or define('Frawst\\DOMAIN', $_SERVER['HTTP_HOST']);
+		defined('Frawst\\ROOT')
+			or define('Frawst\\ROOT', dirname(__FILE__));	
+		defined('Frawst\\APP_ROOT')
+			or define('Frawst\\APP_ROOT', dirname(Frawst\ROOT).DIRECTORY_SEPARATOR.Frawst\APP_NAME);
+		defined('Frawst\\WEB_ROOT')
+			or define('Frawst\\WEB_ROOT', dirname($_SERVER['SCRIPT_NAME']));
+		defined('Frawst\\AJAX_SUFFIX')
+			or define('Frawst\\AJAX_SUFFIX', '___AJAX___');
+		
+		require('Loader.php');
+		
+		use \Frawst\Loader,
+			\Frawst\Exception;
+		
+		function __autoload($class) {
+			Loader::import($class);
 		}
 		
-		$requestData = stripslashes_deep($requestData);
-		$_COOKIE = stripslashes_deep($_COOKIE);
-		// these two lines could be taken out... if they are, accessing
-		// form data with $_GET and $_POST will be inconsistent, but it's
-		// also unneccessary
-		$_GET = stripslashes_deep($_GET);
-		$_POST = stripslashes_deep($_POST);
+		function error_handler($code, $message, $file, $line) {
+			throw new Exception\Language($message, 0, $code, $file, $line);
+		}
+		set_error_handler('error_handler');
 	}
 	
-	$route = isset($_SERVER['PATH_INFO'])
-		? $_SERVER['PATH_INFO']
-		: '';
+	namespace Frawst {
+		/**
+		 * Paths in which libraries can be held.
+		 */
+		Loader::addPath(ROOT, 'Frawst', 'core');
+		Loader::addPath(ROOT.DIRECTORY_SEPARATOR.'vendor', '*', 'core');
+		Loader::addPath(APP_ROOT, 'Frawst', 'app');
+		Loader::addPath(APP_ROOT.DIRECTORY_SEPARATOR.'vendor', '*', 'app');
+		//@todo this probably should be in an app-specific bootstrap since it's specific to Corelativ
+		Loader::addPath(APP_ROOT.DIRECTORY_SEPARATOR.'Model', 'Corelativ\Model', 'app');
 		
-	$headers = array();
-	if(substr($route, -strlen(AJAX_SUFFIX)) == AJAX_SUFFIX) {
-		// hack to get redirected ajax requests working in Firefox
-		$headers['X-Requested-With'] = 'XMLHttpRequest';
-		$route = substr($route, 0, -strlen(AJAX_SUFFIX));
-	} elseif(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
-		$headers['X-Requested-With'] = $_SERVER['HTTP_X_REQUESTED_WITH'];
+		/**
+		 * App-specific bootstrapping
+		 */
+		if(file_exists($file = APP_ROOT.DIRECTORY_SEPARATOR.'bootstrap.php')) {
+			require($file);
+		}
+		
+		date_default_timezone_set(Config::read('general.timezone'));
+		setlocale(LC_ALL, Config::read('general.locale'));
+		
+		$data = null;
+		$mapper = null;
+		$cache = null;
+		if($cacheConfig = Config::read('cache')) {
+			$c = $cacheConfig['controller'];
+			$cache = new $c($cacheConfig);
+		}
+		if($dataConfig = Config::read('data')) {
+			$c = $dataConfig['controller'];
+			$data = new $c($dataConfig, $cache);
+		}
+		if($ormConfig = Config::read('orm')) {
+			$c = $ormConfig['mapper'];
+			$mapper = new $c($ormConfig, $data, $cache);
+		}
+		
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method == 'GET') {
+			$requestData = $_GET;
+		} elseif($method == 'POST') {
+			$requestData = $_POST;
+		} else {
+			$requestData = array();
+			parse_str(file_get_contents('php://input'), $requestData);
+		}
+		
+		// REST hack for browsers that don't support PUT and DELETE methods
+		if(isset($requestData['___METHOD'])) {
+			$method = $requestData['___METHOD'];
+			unset($requestData['___METHOD']);
+		}
+		
+		/**
+		 * I HATE YOU MAGIC QUOTES
+		 */
+		if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
+		 	function stripslashes_deep($value) {
+				$value = (is_array($value)) ?
+					array_map('stripslashes_deep', $value) :
+					stripslashes($value);
+				return $value;
+			}
+			
+			$requestData = stripslashes_deep($requestData);
+			$_COOKIE = stripslashes_deep($_COOKIE);
+			// these two lines could be taken out... if they are, accessing
+			// form data with $_GET and $_POST will be inconsistent, but it's
+			// also unneccessary
+			$_GET = stripslashes_deep($_GET);
+			$_POST = stripslashes_deep($_POST);
+		}
+		
+		$route = isset($_SERVER['PATH_INFO'])
+			? $_SERVER['PATH_INFO']
+			: '';
+			
+		$headers = array();
+		if(substr($route, -strlen(AJAX_SUFFIX)) == AJAX_SUFFIX) {
+			// hack to get redirected ajax requests working in Firefox
+			$headers['X-Requested-With'] = 'XMLHttpRequest';
+			$route = substr($route, 0, -strlen(AJAX_SUFFIX));
+		} elseif(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+			$headers['X-Requested-With'] = $_SERVER['HTTP_X_REQUESTED_WITH'];
+		}
+		
+		$request = new Request($route, $headers, $data, $mapper, $cache);
+		$request->execute($method, $requestData)->send();
 	}
-	
-	$request = new Request($route, $headers, $data, $mapper, $cache);
-	$request->execute($method, $requestData)->send();
