a8032403851f3276f6acd313d2ca96054c01b22b
Daniel Tomasiewicz <dtomasiewicz@gmail.com>
2010-10-17 20:45:06 -0500

    Previous commit failed to remove SimpleCache vendor.

--- vendors/SimpleCache/Engine.php
+++
@@ -1,9 +0,0 @@
-<?php
-	namespace SimpleCache;
-	
-	abstract class Engine {
-		abstract public function exists($name);
-		abstract public function expire($name);
-		abstract public function get($name);
-		abstract public function set($name, $value);
-	}
